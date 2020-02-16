0a73daefcc911cb4ae1ab861be1c5912bec84781
frontstall <deepspiral@yandex.ru>
2020-02-16 17:08:36 +0300

    bootstrap spa

---
+++ server/src/app.module.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'build'),
+    }),
+  ],
+})
+export class AppModule {}
